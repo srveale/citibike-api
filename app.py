fb01020200cf58ac714a612ed4bdb361b75a7bcc
Scott Veale <srveale42@gmail.com>
2019-03-23 21:31:07 -0600

    scrape cronjob and find route

--- app.py
+++ app.py
@@ -1,13 +1,36 @@
 from flask import Flask, jsonify
+from pymongo import MongoClient
+from datetime import datetime, timedelta
+from bson import json_util
+import json
+
 import generate_data
+
 app = Flask(__name__)
+client = MongoClient("mongodb://localhost:27017")
+db=client['citibike-realtime']
 
 @app.route('/')
 def hello_world():
 	return 'Hello, World!'
 
+## purely test, delete this
 @app.route('/bike-data')
 def get_data():
 	data = generate_data.crazy_machine_learning_function()
 	print('data', data)
 	return jsonify(data)
+
+@app.route('/recent-data')
+def get_recent():
+	alldata = db.logs.find({"stationName": "6 Ave & Canal St"})
+	print(list(alldata))
+	two_hours_ago = datetime.now() - timedelta(hours=10)
+	print(two_hours_ago)
+	start = datetime(2014, 9, 24, 7, 51, 4)
+
+	recent_data = db.logs.find({"lastCommunicationTime": { '$lte': start }})
+	# print('data', list(alldata))
+
+
+	return jsonify(list(recent_data))
---
+++ scrape-realtime.py
@@ -0,0 +1,32 @@
+import requests
+from datetime import datetime
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://localhost:27017")
+db=client['citibike-realtime']
+
+##
+## Get data from API
+##
+def get_data():
+	print('getting data')
+	req = requests.get("https://feeds.citibikenyc.com/stations/stations.json")
+
+	req_json = req.json()
+
+	## Times are in NYC timezone
+	req_json['executionTime'] = datetime.strptime(req_json['executionTime'], '%Y-%m-%d %I:%M:%S %p')
+	print(req_json['executionTime'])
+	return req_json
+
+##
+## Store data in Mongo
+##
+def store_data(req_json):
+	for log in req_json['stationBeanList']:
+		log['executionTime'] = req_json['executionTime']
+		db.logs.insert(log)
+
+
+req_data = get_data()
+store_data(req_data)
